7912521e72c585737a2e4a25241ebf7abf417f90
Int314 <15305383+Int314@users.noreply.github.com>
2024-09-18 06:21:29 +0900

    作成処理追加

--- todo_web/src/App.js
+++ todo_web/src/App.js
@@ -1,29 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import Task from './component/Task';
-import { Center, Box, CheckboxGroup, Text } from "@chakra-ui/react";
+import {
+  Flex,
+  Center,
+  Box,
+  CheckboxGroup,
+  Text,
+  Input,
+  Button
+} from "@chakra-ui/react";
+import axios from 'axios';
 
 const App = () => {
-  // タスクの初期値
-  const initialTasks = [
-    { name: "洗濯をする", isDone: false },
-    { name: "電話をする", isDone: true },
-    { name: "料理をする", isDone: true },
-    { name: "Rubyの資格勉強をする", isDone: false },
-    { name: "RailsとReactでTodoアプリを作る", isDone: false },
-  ];
-
   // useStateフックを使用してtasksという状態変数を定義
   // tasksの初期値は空の配列
   // setTasksはtasksの値を更新するための関数
   // 本来は`const [tasks, setTasks] = useState(initialTasks);`と書く
   const [tasks, setTasks] = useState([]);
 
+  const [name, setName] = useState("");
+
+  // axiosを使用してRailsとAPI通信を行い、tasksの値を取得する
+  const fetch = async () => {
+    const res = await axios.get("http://localhost:3001/tasks");
+    console.log(res);
+    setTasks(res.data);
+  };
+
   // コンポーネントの初回レンダリング時に実行
   // 第2引数に空の配列を渡すことで初回レンダリング時のみ実行される
   useEffect(() => {
-    // タスクの初期値をセット
-    setTasks(initialTasks);
+    fetch();
   }, []);
+
+  // タスクを作成する関数
+  const createTask = async () => {
+    await axios.post("http://localhost:3001/tasks", {
+      name: name,
+      is_done: false,
+    });
+    setName("");
+    fetch();
+  };
 
   // タスクのisDoneの状態を更新する関数
   const toggleIsDone = (index) => {
@@ -42,6 +60,18 @@ const App = () => {
           <Box mb="24px">
             <Text fontSize="24px" fontWeight="bold">タスク一覧</Text>
           </Box>
+          <Flex mb="24px">
+            <Input
+              placeholder="タスク名を入力"
+              value={name}
+              onChange={(e) => setName(e.target.value)} // 値が変化したときにnameの値を更新
+            />
+            <Box ml="16px">
+              <Button colorScheme="teal" onClick={createTask}>
+                タスクを作成
+              </Button>
+            </Box>
+          </Flex>
           <CheckboxGroup>
             {/* useStateで定義したtasksの値をmap関数で展開 */}
             {tasks.map((task, index) => {
